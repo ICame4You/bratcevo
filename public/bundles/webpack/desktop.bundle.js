02d7418bebc23cc5daa6bcb9a74617d7ab594cb2
Alexander Shibaylo <sasha.s@w-not.ru>
2018-08-11 18:08:47 +0300

    Верстка

---
+++ public/bundles/webpack/desktop.bundle.js
@@ -0,0 +1,10 @@
+var desktop =
+webpackJsonp_name_([1],[
+/* 0 */
+/***/ (function(module, exports) {
+
+
+
+/***/ })
+],[0]);
+//# sourceMappingURL=desktop.bundle.map
---
+++ public/bundles/webpack/mobile.bundle.js
@@ -0,0 +1,11 @@
+var mobile =
+webpackJsonp_name_([0],[
+/* 0 */,
+/* 1 */
+/***/ (function(module, exports) {
+
+
+
+/***/ })
+],[1]);
+//# sourceMappingURL=mobile.bundle.map
